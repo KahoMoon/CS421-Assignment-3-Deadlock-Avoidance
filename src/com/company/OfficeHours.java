ce9cbbbb262ba10c5d3e763a5b20d91b6450f283
Kaho Moon <kahomoon1@gmail.com>
2022-03-11 19:02:27 -0800

    Trying stop professor from napping if there are multiple students.

--- src/com/company/OfficeHours.java
+++ src/com/company/OfficeHours.java
@@ -4,10 +4,12 @@ import java.util.concurrent.ThreadLocalRandom;
 public class OfficeHours {
 
     boolean professorIsBusy;
+    boolean anotherStudent;
 
     public OfficeHours() {
 
         this.professorIsBusy = false;
+        this.anotherStudent = false;
 
     }
 
@@ -18,10 +20,19 @@ public class OfficeHours {
             synchronized (this){
 
                 if(professorIsBusy){
+
+                    if(ThreadLocalRandom.current().nextInt(0, 2000) % 2 == 0){
+
+                        anotherStudent = true;
+
+                    }
+
                     wait();
+
                 }
 
                 System.out.println("\nA student asks a question.");
+
                 professorIsBusy = true;
 
                 notify();
@@ -37,19 +48,34 @@ public class OfficeHours {
         while(true){
             synchronized (this){
 
-                if(!professorIsBusy){
+                if(!professorIsBusy && !anotherStudent){
                     System.out.println("The professor naps...");
                     wait();
                 }
 
-                System.out.println("The professor answers the student's question.\n");
-                professorIsBusy = false;
-
-                notify();
-                Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 2000));
+                AnswerStart();
+                AnswerDone();
 
             }
         }
+
+    }
+
+    private void AnswerStart(){
+
+        System.out.println("The professor answers the student's question.\n");
+
+    }
+
+    private void AnswerDone() throws InterruptedException{
+
+        if(!anotherStudent){
+            professorIsBusy = false;
+        }
+
+
+        notify();
+        Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 2000));
 
     }
 

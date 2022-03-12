4a483fce5e92de79481c1fd708c422822ba9b09e
Kaho Moon <kahomoon1@gmail.com>
2022-03-11 16:11:33 -0800

    Added student() and working on professor().

--- src/com/company/OfficeHours.java
+++ src/com/company/OfficeHours.java
@@ -2,9 +2,41 @@ package com.company;
 
 public class OfficeHours {
 
-    public void produce() throws InterruptedException{
+    boolean isBusy;
+
+    public OfficeHours() {
+
+        this.isBusy = false;
+
+    }
 
 
+    public void student() throws InterruptedException{
+
+        while(true){
+            synchronized (this){
+
+                while(isBusy){
+                    wait();
+                }
+
+                System.out.print("A student asks a question");
+                isBusy = true;
+
+            }
+        }
+
+    }
+
+    public void professor() throws InterruptedException{
+
+        while(true){
+            synchronized (this){
+
+                while
+
+            }
+        }
 
     }
 

f4804b9c57e0ecbf40d6964fdd2d1833e990c557
Kaho Moon <kahomoon1@gmail.com>
2022-03-11 19:03:41 -0800

    Revert "Changed while loop to if statement."
    This reverts commit 959ba273

--- src/com/company/OfficeHours.java
+++ src/com/company/OfficeHours.java
@@ -4,12 +4,10 @@ import java.util.concurrent.ThreadLocalRandom;
 public class OfficeHours {
 
     boolean professorIsBusy;
-    boolean anotherStudent;
 
     public OfficeHours() {
 
         this.professorIsBusy = false;
-        this.anotherStudent = false;
 
     }
 
@@ -19,20 +17,11 @@ public class OfficeHours {
         while(true){
             synchronized (this){
 
-                if(professorIsBusy){
-
-                    if(ThreadLocalRandom.current().nextInt(0, 2000) % 2 == 0){
-
-                        anotherStudent = true;
-
-                    }
-
+                while(professorIsBusy){
                     wait();
-
                 }
 
                 System.out.println("\nA student asks a question.");
-
                 professorIsBusy = true;
 
                 notify();
@@ -48,34 +37,19 @@ public class OfficeHours {
         while(true){
             synchronized (this){
 
-                if(!professorIsBusy && !anotherStudent){
+                while(!professorIsBusy){
                     System.out.println("The professor naps...");
                     wait();
                 }
 
-                AnswerStart();
-                AnswerDone();
+                System.out.println("The professor answers the student's question.\n");
+                professorIsBusy = false;
+
+                notify();
+                Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 2000));
 
             }
         }
-
-    }
-
-    private void AnswerStart(){
-
-        System.out.println("The professor answers the student's question.\n");
-
-    }
-
-    private void AnswerDone() throws InterruptedException{
-
-        if(!anotherStudent){
-            professorIsBusy = false;
-        }
-
-
-        notify();
-        Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 2000));
 
     }
 
